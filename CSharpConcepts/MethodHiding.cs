1ef40fdc5722224aada051df5ab757063224a896
Anshul Dahiya <anshul.rts@gmail.com>
2020-05-03 16:24:24 +0530

    Initial Commit and Method Hiding

---
+++ CSharpConcepts/MethodHiding.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSharpConcepts
+{
+    public class MethodHiding
+    {
+        public void start()
+        {
+            Parent p = new Parent();
+            Son s = new Son();
+
+            p.display();
+            s.display();
+
+            p.Show();
+            s.Show();
+
+            Parent p1 = new Son();
+            p1.display();
+            p1.Show();
+        }
+    }
+
+    public class Parent
+    {
+        int x = 10;
+
+        public void display()
+        {
+            Console.WriteLine("In Parent. x = {0}", x);
+        }
+
+        public void Show()
+        {
+            Console.WriteLine("Show In Parent. x = {0}", x);
+        }
+    }
+
+    class Son : Parent
+    {
+        int x = 20;
+
+        // Even though you can declare a method with same name as that in parent
+        // It will give a warning only and will call respective method.
+        // But still better to use new keyword like in Show()
+        public void display()
+        {
+            Console.WriteLine("In Son. x = {0}", x);
+        }
+
+        public new void Show()
+        {
+            Console.WriteLine("Show In Son. x = {0}", x);
+        }
+    }
+}
---
+++ CSharpConcepts/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace CSharpConcepts
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("\n\n\n\n-----Menu Driven Program--------");
+                Console.WriteLine("1. Exit");
+                Console.WriteLine("2. Method Hiding");
+
+                Console.WriteLine("\nEnter Choice");
+                string x = Console.ReadLine();
+
+                switch (Convert.ToInt32(x))
+                {
+                    case 1:
+                        System.Environment.Exit(0);
+                        break;
+                    case 2:
+                        MethodHiding m = new MethodHiding();
+                        m.start();
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+    }
+}
